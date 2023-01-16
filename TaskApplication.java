1d136b34d5072e4231ff26c48b88ac09ea4400a3
Joshua <joshuahng17@gmail.com>
2023-01-16 16:28:31 -0500

    Updated Add Task

--- TaskApplication.java
+++ TaskApplication.java
@@ -30,45 +30,60 @@ class TaskBean{
 
 public class TaskApplication{
 	
+	static TaskBean[] TaskArray = new TaskBean[100];
+	static int TaskSize = 0;
+	
 	public static void main(String[] args){
-		TaskBean[] TaskArray = new TaskBean[100];
-		int numberOfTasks = 0;
-		System.out.println("*****Menu******");
-		System.out.println("1. Add a new task");
-		System.out.println("2. Update a task");
-		System.out.println("3. Delete a Task");
-		System.out.println("4. Search for a task");
-		System.out.println("Please enter your choice!");
-		System.out.println("Press 0 to exit");
-		int choice;
+		
 		Scanner sc=new Scanner(System.in);
-		choice=sc.nextInt();
-		while(choice !=0){
+		int choice;
+		do {
+			System.out.println("*****Menu******");
+			System.out.println("1. Add a new task");
+			System.out.println("2. Update a task");
+			System.out.println("3. Delete a Task");
+			System.out.println("4. Search for a task");
+			System.out.println("Please enter your choice!");
+			System.out.println("Press 0 to exit");
+			choice=sc.nextInt();
 			switch(choice) {
 				case 1:
-					TaskArray[numberOfTasks] = new TaskBean(++numberOfTasks, "New Task");
+					System.out.println("Name of task: ");
+					String name = sc.nextLine();
+					AddTask(name);
 					System.out.println("Added a new task");
 					break;
 				case 2:
-					TaskArray[(numberOfTasks - 1)].setTaskName("New Name");
+					TaskArray[(TaskSize - 1)].setTaskName("New Name");
 					System.out.println("Updated a task");
 					break;
 				case 3:
-					TaskArray[numberOfTasks--] = null;
+					TaskArray[TaskSize--] = null;
 					System.out.println("Deleted a task");
 					break;
 				case 4:
-					for(int i=0;i<numberOfTasks;i++){
+					for(int i=0;i<TaskSize;i++){
 						System.out.println(TaskArray[i].getTaskName());
 					}
 					break;
+				case 0:
+					System.out.println("Goodbye");
 				default:
 					System.out.println("Invalid Input");
 					break;
 			}
-			choice = sc.nextInt();
-		}
+		} while(choice != 0);
 		sc.close();
+		return;
+	}
+	
+	public static void AddTask(String name){
+		TaskBean newTask = new TaskBean(TaskSize, name);
+		for(int i=0;i<TaskArray.length;i++){
+			if(TaskArray[i] == null)
+				TaskArray[i] = newTask;
+		}
+		TaskSize++;
 		return;
 	}
 }
